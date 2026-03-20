1afb4646290466c6fdf93bc4be5c9db69e79b37c
Roddie <Rudseller33@gmail.com>
2026-03-20 12:48:33 +0300

    up to go on lab5

---
+++ lab5/post_preview.php
@@ -0,0 +1,8 @@
+<div>
+  <h3><?= $post['title'] ?></h3>
+  <h4><?= $post['subtitle'] ?></h4>
+  <span><?= $post['author'] ?></span>
+  <a title='<?= $post['title'] ?>' href='/post?id=<?= $post['id'] ?>'>
+    <?= $post['subtitle'] ?>
+  </a>
+</div>
