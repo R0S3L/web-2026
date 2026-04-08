e639310c2a1a101da1e03e892c593035e8377181
Roddie <Rudseller33@gmail.com>
2026-04-08 10:17:36 +0300

    changes

---
+++ lab7/data/sql/table/create_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `blog`.`user` (
+  `id_user` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `user_surname` varchar(50) NOT NULL,
+  `user_description` varchar(90) DEFAULT NULL,
+  `user_image` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `blog`.`post` (
+  `id_post` int unsigned NOT NULL AUTO_INCREMENT,
+  `post_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id_user` int unsigned NOT NULL,
+  `post_image` varchar(100) NOT NULL,
+  `post_description` varchar(100) DEFAULT NULL,
+  `post_likes` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_post`),
+  KEY `user_post_idx` (`id_user`),
+  CONSTRAINT `post_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
