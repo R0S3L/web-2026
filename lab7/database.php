4a9cafec063252fcea9aeadfa88ef04bf0354857
Соколов Родион <Rudseller33@gmail.com>
2026-04-21 20:11:25 +0300

    Подключение к бд

--- lab7/database.php
+++ lab7/database.php
@@ -1,18 +1,43 @@
 <?php
 
-function connectDB(): PDO {
-    $dsn = 'mysql:host=127.0.0.1; dbname=blog';
+function connectDB(): PDO
+{
+    $dsn = 'mysql:host=127.0.0.1;dbname=blog;charset=utf8mb4';
     $user = 'root';
     $password = '12345';
 
-    return new PDO($dsn, $user, $password)
+    return new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
 }
 
-function findPostInDB(PDO $connection, int $id): ?array{
-    $query = <<<SQL
+function findPostInDB(PDO $connection, int $id): ?array
+{
+    $stmt = $connection->prepare("
         SELECT
-            id_post, post_date, id_user, post_image, post_description, post_likes
-        FROM post
-        WHERE id = $id 
-    SQL;
+            p.id_post,
+            p.post_date,
+            p.post_image,
+            p.post_description,
+            p.post_likes,
+            u.id_user,
+            u.user_name,
+            u.user_surname,
+            u.user_image
+        FROM post AS p
+        INNER JOIN user AS u ON p.id_user = u.id_user
+        WHERE p.id_post = :id
+        LIMIT 1
+    ");
+
+    $stmt->execute([':id' => $id]);
+    $row = $stmt->fetch();
+
+    return $row !== false ? $row : null;
 }
+
+function formatPostDate($dateString) {
+    return date('d.m.Y H:i', strtotime($dateString));
+}
+
---
+++ lab7/home/index.php
@@ -0,0 +1,81 @@
+<?php
+require_once __DIR__ . '/../includes/database.php';
+
+$pdo = connectDB();
+
+$ids = $pdo->query("SELECT id_post FROM post ORDER BY post_date DESC")->fetchAll(PDO::FETCH_COLUMN);
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body class="block">
+
+    <nav class="side-panel">
+        <button class="side-panel-button">
+            <img class="side-panel-icon" src="../images/icons/home.png" width="24" height="24" alt="Дом">
+            <img class="icons dot" src="../images/icons/dot.png" alt="Dot">
+        </button>
+        <button class="side-panel-button">
+            <img class="side-panel-icon" src="../images/icons/user.png" width="24" height="24" alt="Профиль">
+        </button>
+        <button class="side-panel-button">
+            <img class="side-panel-icon" src="../images/icons/plus.png" width="24" height="24" alt="Создать">
+        </button>
+    </nav>
+
+    <div class="scroll_bar">
+
+        <?php if (empty($ids)): ?>
+            <p>Постов пока нет.</p>
+        <?php endif; ?>
+
+        <?php foreach ($ids as $id): ?>
+            <?php
+                $post = findPostInDB($pdo, (int)$id);
+                if ($post === null) continue;
+
+                $fullName    = htmlspecialchars($post['user_name'] . ' ' . $post['user_surname']);
+                $userImage   = htmlspecialchars($post['user_image'] ?? 'default_user.png');
+                $postImage   = htmlspecialchars($post['post_image']);
+                $description = htmlspecialchars($post['post_description'] ?? '');
+                $likes       = (int)$post['post_likes'];
+                $dateLabel   = formatPostDate($post['post_date']);
+            ?>
+            <div class="post">
+
+                <div class="user_bar">
+                    <img class="user_photo"
+                         src="../images/<?= $userImage ?>"
+                         alt="<?= $fullName ?>">
+                    <h2 class="nickname"><?= $fullName ?></h2>
+                    <img class="icons pen" src="../images/icons/pen.svg" alt="Редактировать">
+                </div>
+
+                <div>
+                    <img class="photo_post"
+                         src="../images/<?= $postImage ?>"
+                         alt="Пост">
+                </div>
+
+                <div class="react_border">
+                    <img class="react_border likes" src="../images/icons/like.png" alt="Лайки">
+                    <h2 class="react_border counter"><?= $likes ?></h2>
+                </div>
+
+                <div>
+                    <h3 class="comments"><?= $description ?></h3>
+                    <h3 class="comments description">ещё</h3>
+                    <h3 class="comments description"><?= $dateLabel ?></h3>
+                </div>
+
+            </div>
+        <?php endforeach; ?>
+
+    </div>
+
+</body>
+</html>
